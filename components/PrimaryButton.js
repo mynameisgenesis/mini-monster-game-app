dd321f8c5322d8b9392173fc7f67b46eedff38ad
Jessica Murillo <jmurillo@quantum-intl.com>
2022-10-05 14:54:54 -0600

    Add button styling for both Android and iOS

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log('Pressed!');
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        android_ripple={{ color: '#FF5EA5' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 30,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#FF74B1',
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+    elevation: 2,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
