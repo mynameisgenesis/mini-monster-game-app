e394cfa2662215ef98308f3a5240adf84c4c2ae4
Jessica Murillo <jmurillo@quantum-intl.com>
2022-10-05 10:26:28 -0600

    Add planned screen files

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from 'react-native';
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      {/* Button goes here */}
+    </View>
+  );
+}
+
+export default StartGameScreen;
