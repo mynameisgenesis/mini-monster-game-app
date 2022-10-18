64db075407c9212196f51d35e201c1c76830a33f
Genesis Murillo <genesis.mrll@gmail.com>
2022-10-18 14:16:37 -0600

    Add expo icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { View, StyleSheet, Alert } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
@@ -63,12 +64,12 @@ function GreenScreen({ userNumber, gameOverHandler }) {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "higher")}>
-              ➕
+              <Feather name="minus" size={32} />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              ➖
+              <Feather name="plus" size={32} />
             </PrimaryButton>
           </View>
         </View>
